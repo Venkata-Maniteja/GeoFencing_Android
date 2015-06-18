445848ba93907c02029f9541943fde0bfbfb1be0
Venkata Maniteja <tejanvm92@gmail.com>
2015-06-18 15:48:24 -0400

    Added the sharedprefs unit testing and textfield not empty unit testing

--- app/src/test/java/com/example/geofencing/test/locationtest.java
+++ app/src/test/java/com/example/geofencing/test/locationtest.java
@@ -3,6 +3,7 @@ package com.example.geofencing.test; /**
  */
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationManager;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 
 import com.example.geofencing.R;
 import com.example.geofencing.Settings;
+import com.example.geofencing.Util;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,9 +21,13 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLocationManager;
+import org.robolectric.shadows.ShadowPreferenceManager;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
@@ -38,6 +44,9 @@ public class locationtest  {
   public void setUp() {
       mainActivity = new Settings();
       mainActivity=Robolectric.buildActivity(Settings.class).create().get();
+      btnGetCoords = (Button) mainActivity.findViewById(R.id.getlocation);
+      latitudeTextField = (EditText) mainActivity.findViewById(R.id.latitude);
+      longitueTextField = (EditText) mainActivity.findViewById(R.id.longitude);
 
   }
     @After
@@ -45,7 +54,7 @@ public class locationtest  {
     }
 
   @Test
-    public void shouldReturnTheLatestLocation() {
+  public void shouldReturnTheLatestLocation() throws Exception {
         LocationManager locationManager = (LocationManager)
                 Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
         ShadowLocationManager shadowLocationManager = Robolectric.shadowOf(locationManager);
@@ -53,9 +62,9 @@ public class locationtest  {
 
         shadowLocationManager.simulateLocation(expectedLocation);
 
-      System.out.print("expected location is " + expectedLocation.getLatitude() + expectedLocation.getLongitude());
+//      System.out.print("expected location is " + expectedLocation.getLatitude() + expectedLocation.getLongitude());
         Location actualLocation = mainActivity.latestLocation();
-      System.out.print("actual location is " + actualLocation.getLatitude() + actualLocation.getLongitude());
+//      System.out.print("actual location is " + actualLocation.getLatitude() + actualLocation.getLongitude());
         assertEquals(expectedLocation, actualLocation);
     }
 
@@ -69,13 +78,46 @@ public class locationtest  {
 
 
     @Test
-    public void textFieldCheck(){
+    public void textFieldButtonExistCheck() throws Exception {
 
-        btnGetCoords = (Button) mainActivity.findViewById(R.id.getlocation);
-        latitudeTextField = (EditText) mainActivity.findViewById(R.id.latitude);
+
+        // Verifies the button and text field exist
+        assertThat(btnGetCoords, notNullValue());
+        assertNotNull(latitudeTextField);
+        assertNotNull(longitueTextField);
         assertThat((String) btnGetCoords.getText(), equalTo("Get Current Location"));
     }
 
+    @Test
+    public void textFieldNotNullCheck() throws Exception {
 
+
+        assertNotNull(btnGetCoords);
+
+        // Trigger a click on the button
+        btnGetCoords.performClick();
+        String result1 = latitudeTextField.getText().toString();
+        String result2 = longitueTextField.getText().toString();
+        assertThat(latitudeTextField, notNullValue());
+        assertThat(longitueTextField, notNullValue());
+        assertThat(result1, containsString("No"));  //textfiled is not empty after button click
+        assertThat(result2, containsString("No"));
+        System.out.print("The text is " + latitudeTextField.getText().toString());
+
+    }
+
+    @Test
+    public void sharedPrefsTest() {
+
+//        here im just checking whether shared prefs is accpeting data or not
+        SharedPreferences sharedPreferences = ShadowPreferenceManager.getDefaultSharedPreferences(Robolectric.application.getApplicationContext());
+        sharedPreferences.edit().putString(Util.LONGITUDE, "12345").commit();
+
+        SharedPreferences sharedPreferences2 = Robolectric.application.getSharedPreferences("myprefs", Context.MODE_PRIVATE);
+        sharedPreferences2.edit().putString(Util.LONGITUDE, "12345").commit();
+        mainActivity = Robolectric.buildActivity(Settings.class).create().get();
+//        btnGetCoords.performClick();
+        System.out.print("the lat text is " + latitudeTextField.getText().toString().trim());
+    }
 
 }
